1fe8058b874f21aa4c7b39ca7f9d59e204b6edfa
vincenzobusiello <126822842+VincenzoBusiello@users.noreply.github.com>
2026-01-24 16:27:45 +0100

    Update Exam_01_28.R

--- Exam_01_28.R
+++ Exam_01_28.R
@@ -8,6 +8,7 @@ library(gglot2)# for graphs
 library(viridis) # alternative color palettes 
 library(patchwork) # for multiple grapsh on a single plot
 
+### 1st Case Study - Bourgneuf Bay, France - Zostera noltei
 
 # setting the working directory 
 setwd("C:/SEexam/francia1")
@@ -43,4 +44,24 @@ for (j in seq_along(images))
 ## let's transform it in a spat raster! 
 fb_r <- rast(fb)
 fb_r
+
+# plotting the fb_r in true color
 im.plotRGB(fb_r, 1,2,3)
+# plotting the fb_r whit NIR band in red
+im.plotRGB(fb_r, 4,2,3)
+
+dev.off()
+
+par(mfrow=c(1,2))
+im.plotRGB(fb_r, 1,2,3)
+im.plotRGB(fb_r, 4,2,3)
+
+
+# calculating NDVI 
+fb_dif = fb_r[[4]] - fb_r[[1]]
+fb_sum = fb_r[[4]] + fb_r[[1]]
+fbNDVI = fb_dif / fb_sum
+
+par(mfrow=c(2,1))
+plot(fb_r, col=viridis(100))
+plot(fbNDVI, col=viridis(100))
