bfa45cab05e7eee31750b8298f44634caecda134
vincenzobusiello <126822842+VincenzoBusiello@users.noreply.github.com>
2026-01-24 13:32:47 +0100

    Update Exam_01_28.R

--- Exam_01_28.R
+++ Exam_01_28.R
@@ -1,6 +1,5 @@
 # The purpose of this code is to monitor the progress of two types of vegetation over the years 
-# in order to determine whether there has been any environmental change due to anthropogenic causes 
-# and/or climate change. 
+# in order to determine whether there has been any environmental.
 
 
 library(terra) # used for spatial data life raster or vector data
@@ -11,20 +10,37 @@ library(patchwork) # for multiple grapsh on a single plot
 
 
 # setting the working directory 
-setwd("C:/SEexam/Batt Reef")
+setwd("C:/SEexam/francia1")
 
-# importing images 
-br1 <- rast("Breef04.tiff")
-br2 <- rast("Breef03.tiff")
-br3 <- rast("Breef02.tiff")
-br4 <- rast("Breef08.tiff")
+# checking if there are all the needed files in the selected directory
 
-# creating a single images whit 4 bands, as follows
+files <- list.files()
+for (i in files) 
+    {
+      print (i)
+        if (i == tail(files, n=1))
+          {
+            break
+          }    
+    }
+
+# creating a single object whit 4 images, which every one is a specific band, as follows:
 # 1) Red
 # 2) Blue
 # 3) Green
-# 4) NearInfraRed - NIR
-br <- c(br1, br2, br3, br4)
+# 4) NIR
 
-# printing the true color image
-im.plotRGB(br, 1,2,3)
+images <- c("fb4.tiff", "fb3.tiff", "fb2.tiff", "fb8.tiff")
+
+fb <- vector("list", length(images))
+
+for (j in seq_along(images))
+  {
+    fb[[j]] <- rast(images[j])
+  }
+## at this point, fb is a list of data. 
+
+## let's transform it in a spat raster! 
+fb_r <- rast(fb)
+fb_r
+im.plotRGB(fb_r, 1,2,3)
